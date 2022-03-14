0ba6706f94db5e80baca34d83e1fcafb41cbcfcc
sam <70492417+dsblack0@users.noreply.github.com>
2022-03-14 06:44:01 +0000

    challenge3-matrix complete

--- challenges/Matrix.java
+++ challenges/Matrix.java
@@ -21,6 +21,35 @@ public class Matrix {
                 { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
     }
 
+    public String toString() {
+      StringBuilder matrixBuild = new StringBuilder("");
+      for (int row=0; row < matrix.length; row++) {
+        StringBuilder rowBuild = new StringBuilder("");
+        for (int col=0; col < matrix[row].length; col++) {
+          if (matrix[row][col] < 0) {
+            rowBuild.append("  ");
+          } else {
+           rowBuild.append(Integer.toHexString(matrix[row][col]) + " "); 
+          }
+        }
+        matrixBuild.append(rowBuild + "\n");
+      }
+
+     for (int row=matrix.length-1; row >=0 ; row--) {
+        StringBuilder rowBuild = new StringBuilder("");
+        for (int col=matrix[row].length-1; col >= 0; col--) {
+          if (matrix[row][col] < 0) {
+            rowBuild.append("  ");
+          } else {
+           rowBuild.append(Integer.toHexString(matrix[row][col]) + " "); 
+          }
+        }
+        matrixBuild.append("\n" + rowBuild);
+      }
+      
+      return matrixBuild.toString();
+    }
+
     // tester method for matrix formatting
     public static void main(String[] args) {
         Matrix m0 = new Matrix(keypad());
