8dfbc69869650b83f2d675328df1cc2f60cca3e5
sam <70492417+dsblack0@users.noreply.github.com>
2022-04-04 16:28:21 +0000

    bubble sort comments

--- challenges/BubbleSort.java
+++ challenges/BubbleSort.java
@@ -1,15 +1,20 @@
 public class BubbleSort extends SortData {
 
     public BubbleSort() {
+        //inherit method from SortData
         super();
     }
 
     public void sort() {
+        //repeats sort starting from each index
         for(int i=0; i< sortList.length -1; i++) {
+            //go through each index in the list
             for(int j=1; j<sortList.length -1; j++) {
                 comparisons++;
+                //compare integer to previous integer
                 if (sortList[j-1] > sortList[j]) {
                     swaps++;
+                    //switch integers if second is greater than first
                     int x = sortList[j];
                     sortList[j] = sortList[j-1];
                     sortList[j-1] = x;
