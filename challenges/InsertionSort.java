3fac5b355adaa5a2fc23547131b0a020a995f8a4
sam <70492417+dsblack0@users.noreply.github.com>
2022-04-04 16:38:39 +0000

    insertion + merge sort comments

--- challenges/InsertionSort.java
+++ challenges/InsertionSort.java
@@ -5,18 +5,23 @@ public class InsertionSort extends SortData {
     }
 
     public void sort() {
+      //repear sort from each integer in list
         for (int i=1; i<sortList.length; ++i) {
+            //set starting "key"
             int x = sortList[i];
             int j = i-1;
 
             comparisons++;
+            //loop as long as integer is smaller than previous one
             while (j >=0 && sortList[j] > x) {
+                //move integer back one index if smaller than that previous index
                 sortList[j+1] = sortList[j];
                 j--;
             }
             if (sortList[j+1] != x) {
                 swaps++;
             }
+            //set next integer as new key
             sortList[j+1] = x;
         }
     }
--- challenges/MergeSort.java
+++ challenges/MergeSort.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 public class MergeSort extends SortData {
 
     public MergeSort() {
+      //doesn't inherit super class as sort() method has parameters
         for (int i=0; i<12; i++) {
             Instant start = Instant.now();
             sort(sortList, sortList.length);
@@ -33,29 +34,34 @@ public class MergeSort extends SortData {
     }
 
     public void sort(int[] list, int e) {
+        //return array if it has only one integer
         if (e < 2) {
             return;
         }
+        //find mindpoint of array
         int m = e/2;
         int[] one = new int[m];
         int[] two = new int[e-m];
-
+        //split initial array into two arrays at the midpoint
         for (int i=0; i < one.length; i++) {
             one[i] = list[i];
         }
         for (int i=0; i < two.length; i++) {
             two[i] = list[i+m];
         }
-
+        //continue to split the array until all arrays hold only 1 integer
         sort(one, m);
         sort(two, e-m);
+        //sort integers as pairs of arrays are merged into the combined list
         mergeSort(list, one, two, m, e-m);
     }
 
     public void mergeSort(int[] list, int[] one, int[] two, int o, int t) {
         int i=0, j=0, k=0;
+        //loop as long as both separated arrays have integers in them
         while (i < o && j < t) {
             comparisons++;
+            //add smaller integer out of the two arrays into the combined array
             if (one[i] <= two[j]) {
                 list[k] = one[i];
                 i++;
@@ -67,13 +73,13 @@ public class MergeSort extends SortData {
             }
             k++;
         }
-
+        //add any leftover integers from first split array
         while (i < o) {
             list[k] = one[i];
             i++;
             k++;
         }
-
+        //add any leftover integers from second split array
         while (j < t) {
             list[k] = two[j];
             j++;
